be04bd5ee1a22beaec5197ca89a502ba6deff6b5
bdemedeiros <bdemedeiros@fieldmuseum.org>
2023-08-23 13:31:00 -0500

    correct error in SRA query

--- varKoder_tests/all_SRA/6_query_SRA.sh
+++ varKoder_tests/all_SRA/6_query_SRA.sh
@@ -3,17 +3,17 @@
 
 conda deactivate
 conda activate varKoder
+export CUDA_VISIBLE_DEVICES=3
 
 #create folder with query images (i. e. those chosen as validation)
 mkdir -p varkoder_query_images
-#cat validation_samples_SL.txt | tr ',' '\n' | xargs -I {} find varkoder_images_SRA/ -name "{}@*" -exec ln -sf {} varkoder_query_images/ \;
 
-cat validation_samples_SL.txt | tr ',' '\n' | xargs -L 100 -I {} bash -c 'for sample in "$@"; do for file in $(find varkoder_images_SRA/ -name "${sample}@*"); do ln -sf "$file" varkoder_query_images/; done; done' bash {}
+cat validation_samples_SL.txt | tr ',' '\n' | xargs -L 500 -I {} bash -c 'for sample in "$@"; do for file in $(find varkoder_images_SRA/ -name "${sample}@*"); do ln -sf ../"$file" varkoder_query_images/; done; done' bash {}
 
 #now do query
 VARKODER_PATH=/home/bdemedeiros/varKoder_test/varKoder/varKoder.py
 
-time python $VARKODER_PATH query --overwrite -I -b 300 varkoder_trained_model_ML/trained_model.pkl varkoder_images_SRA varkoder_query_results
+time python $VARKODER_PATH query --overwrite -I -b 300 varkoder_trained_model_ML/trained_model.pkl varkoder_query_images varkoder_query_results
 
 
 exit
