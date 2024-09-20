d87fcba824cc330b39efb47c65b3449c0f79bd1f
peterjflynn <peterjflynn91@gmail.com>
2024-09-20 19:49:33 -0400

    Add files via upload
    other datasets with updated code for VarKoder with cgr and memory usage code.

---
+++ varKoder_tests/other_datasets/memory_usage/Cora/Mem_Cora_Train.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+
+# Start the process in the background and capture its PID
+varKoder train cgr_Cora cora_model_cgr &
+pid=$!
+
+# Create a log file in the home directory
+logfile=$(mktemp ~/memory.log.XXXX)
+start=$(date +%s)
+
+# Function to clean up in case of an error
+cleanup() {
+    if [[ -n "$logfile" ]]; then
+        rm -f "$logfile"
+    fi
+    exit 1
+}
+
+# Trap errors to clean up
+trap cleanup ERR
+
+# Get the process' memory usage and run until `ps` fails, which it will do when the PID cannot be found any longer
+while mem=$(ps -o rss= -p "$pid" 2>/dev/null); do
+    time=$(date +%s)
+
+    # Print the time since starting the program followed by its memory usage
+    printf "%d %s\n" $((time-start)) "$mem" >> "$logfile"
+
+    # Sleep for a tenth of a second
+    sleep .1
+done
+
+# Check if the log file has contents before printing the path
+if [[ -s "$logfile" ]]; then
+    printf "Find the log at %s\n" "$logfile"
+else
+    printf "No memory usage was recorded. Process might have terminated immediately.\n"
+    rm -f "$logfile"
+fi
